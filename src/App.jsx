16c1ecc31c7f70a431524ccdc861e328a185b502
anakin-dabir <anakindabir@gmail.com>
2023-12-01 08:46:13 +0500

    Danku | Finale

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+/*
+ * /src/App.js
+ * Saturday, September 30th 2023, 12:42 am
+ */
+
+import Care from './components/Care';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Products from './components/Products';
+import Reference from './components/Reference';
+import Services from './components/Services';
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Products />
+      <Reference />
+      <Care />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// /src/index.jsx
+// Saturday, September 30th 2023, 12:42 am
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
